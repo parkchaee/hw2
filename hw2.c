ed8d9437309bcd646b6ce32473a26981caa5bdd4
parkchaee <87425720+parkchaee@users.noreply.github.com>
2021-10-05 18:43:04 +0900

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -2,13 +2,7 @@
 
 int main(void)
 {
-    /*double su;
-    double mete;
-    printf("Please enter kilometers.:");
-    scanf_s("%lf", &su);
-    mete = 1000 / 1609 * su;
-    printf("%2lf km is equal to %2lf miles\n", su, mete);
-    return 0;*/
+    
     double kilo;
     double mete;
     printf("Please enter kilometers.:");
